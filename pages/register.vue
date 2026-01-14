94fdb5e2eb248a2a3bbdc4a75eb70c12270509ee
DevPieter <pieter@devpieter.nl>
2026-01-14 11:06:04 +0100

    Improve colors on login and register forms

--- pages/register.vue
+++ pages/register.vue
@@ -56,45 +56,45 @@ const handleSubmit = async (e: Event) => {
 
       <form @submit="handleSubmit" class="flex flex-col mt-4">
 
-        <div class="space-y-2">
-          <div class="w-full flex flex-col">
+        <div class="space-y-4">
+          <div class="w-full flex flex-col space-y-2">
             <input
                 v-model="username"
                 required
                 type="text"
                 placeholder="gebruikersnaam"
-                class="mt-2 p-2 border border-gray-400 rounded"
+                class="p-2 border bg-[#1f1f1f] border-gray-400 rounded"
             />
             <input
                 v-model="pronouns"
                 required
                 type="text"
                 placeholder="voornaamwoorden"
-                class="mt-2 p-2 border border-gray-400 rounded"
+                class="p-2 border bg-[#1f1f1f] border-gray-400 rounded"
             />
           </div>
 
-          <div class="w-full flex flex-col">
+          <div class="w-full flex flex-col space-y-2">
             <input
                 v-model="email"
                 required
                 type="text"
                 placeholder="e-mail"
-                class="mt-4 p-2 border border-gray-400 rounded"
+                class="p-2 border bg-[#1f1f1f] border-gray-400 rounded"
             />
             <input
                 v-model="password"
                 required
                 type="password"
                 placeholder="wachtwoord"
-                class="mt-2 p-2 border border-gray-400 rounded"
+                class="p-2 border bg-[#1f1f1f] border-gray-400 rounded"
             />
             <input
                 v-model="confirmPassword"
                 required
                 type="password"
                 placeholder="bevestig wachtwoord"
-                class="mt-2 p-2 border border-gray-400 rounded"
+                class="p-2 border bg-[#1f1f1f] border-gray-400 rounded"
             />
           </div>
         </div>
